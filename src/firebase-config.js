a95ca4055497945cc2a950266e3ee4ea265dfe03
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-06-06 00:57:01 +0900

    impl: add firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+"use strict";
+// note: あなたの設定値を入力してください
+class FirebaseConfig {
+    constructor() {
+        this.apiKey = "";
+        this.authDomain = "";
+        this.projectId = "";
+        this.storageBucket = "";
+        this.messagingSenderId = "";
+        this.appId = "";
+        this.measurementId = "";
+    }
+}
