f18b784b67b809c71360643167124510c05b1dc6
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2022-01-23 18:36:54 +0900

    impl: youtube utility のテストを追加

---
+++ src/__tests__/utility/youtube.test.ts
@@ -0,0 +1,11 @@
+import {Youtube} from "../../utility/youtube";
+
+test('getVideoIdFromUrl:単発動画URLからビデオIDを取得', () => {
+    expect(Youtube.getVideoIdFromUrl('https://www.youtube.com/watch?v=Z4kkTI4OFqs'))
+        .toBe('Z4kkTI4OFqs')
+})
+
+test('getVideoIdFromUrl:リストの動画URLからビデオIDを取得', () =>{
+    expect(Youtube.getVideoIdFromUrl('https://www.youtube.com/watch?v=Y92XOjNxBXs&list=RDCMUCzXjPL7zo0bxhOYDxLJ9YEg&index=2'))
+        .toBe('Y92XOjNxBXs')
+})
--- src/utility/youtube.ts
+++ src/utility/youtube.ts
@@ -14,6 +14,7 @@ export class Youtube
     }
 
     static getCurrentCaptionLanguage() : string {
+        // note: フォーマット例: <div id="label-text" class="style-scope yt-dropdown-menu">英語</div>
         const captionLanguageDOM = document.getElementById("label-text")
         const language = captionLanguageDOM != null ? captionLanguageDOM.innerText : ""
         return language
